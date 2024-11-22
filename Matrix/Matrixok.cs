80d9613f866ff1b2cd49eeee4495a552106e02f3
oze.laura <ozelara@gmail.com>
2024-11-22 14:22:23 +0100

    Add project files.

---
+++ Matrix/Matrixok.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    internal class Matrixok
+    {
+        private double[,] matrix;
+
+        public Matrixok(int m, int n)
+        {
+            matrix = new double[m, n];
+        }
+
+        public Matrixok(int n)
+        {
+            matrix = new double[n, n];
+        }
+
+        public Matrixok()
+        {
+            matrix = new double[3, 3];
+        }
+
+        public static Matrixok Sum(Matrixok A, Matrixok B)
+        {
+            if (A.Rows != B.Rows || A.Columns != B.Columns)
+            {
+                throw new ArgumentException("A két mátrix nem ugyanakkora méretű!");
+            }
+
+            Matrixok result = new Matrixok(A.Rows, A.Columns);
+
+            for (int i = 0; i < A.Rows; i++)
+            {
+                for (int j = 0; j < A.Columns; j++)
+                {
+                    result.matrix[i, j] = A.matrix[i, j] + B.matrix[i, j];
+                }
+            }
+
+            return result;
+        }
+
+
+        public void Fill(double value)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = value;
+                }
+            }
+        }
+
+        public int Rows
+        {
+            get { return matrix.GetLength(0); }
+        }
+
+        public int Columns
+        {
+            get { return matrix.GetLength(1); }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using Matrix;
+
+class Program
+{
+    static void Main()
+    {
+        Matrixok m1 = new Matrixok();
+        m1.Fill(1);
+        Console.WriteLine("M1:");
+        m1.Print();
+    }
+}
